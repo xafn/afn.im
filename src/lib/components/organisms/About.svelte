a6d15e9cf55134d1eb7595ee3bd1701b7bdf6c99
afnzmn <afnzmn@gmail.com>
2022-08-06 01:10:34 -0400

    grid template columns

--- src/lib/components/organisms/About.svelte
+++ src/lib/components/organisms/About.svelte
@@ -8,9 +8,7 @@
             <RichPresenceHost />
         </div>
         <div class="text">
-            <h6>
-                Hello funny people in my screen ☺. I'm a 15 year old digital artist and graphic/UI designer based in Canada. I’ve taken art seriously since 2017, and have been doodling goofy anime characters since 2020. I’ve also done some design work in open-source projects. Recently, however, I’ve grown a knack in programming. Currently learning Python, and JS -> Svelte. Hope to move onto Flutter one day. Don’t look into it though, it’s not even that interesting.
-            </h6>
+            <h6>Hello funny people in my screen ☺. I'm a 15 year old digital artist and graphic/UI designer based in Canada. I’ve taken art seriously since 2017, and have been doodling goofy anime characters since 2020. I’ve also done some design work in open-source projects. Recently, however, I’ve grown a knack in programming. Currently learning Python, and JS -> Svelte. Hope to move onto Flutter one day. Don’t look into it though, it’s not even that interesting.</h6>
         </div>
     </div>
 </section>
@@ -18,7 +16,7 @@
 <style>
     section > div {
         display:grid;
-		grid-template-columns: 1fr 1fr;
+		grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
         flex-direction: column;
     }
 
