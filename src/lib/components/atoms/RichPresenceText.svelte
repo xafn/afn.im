c3c66e8ec06f62b7f41a9f1377990711de8c6381
af-n <733093@gmail.com>
2022-08-10 03:16:29 -0400

    extremely bad rich presence wip

--- src/lib/components/atoms/RichPresenceText.svelte
+++ src/lib/components/atoms/RichPresenceText.svelte
@@ -1,29 +1,71 @@
 <script>
     import { onMount } from "svelte";
-    const fetchLanyardData = fetch("https://api.lanyard.rest/v1/users/420043923822608384")
-        .then(response => response.json())
-        .then((data) => { 
-            return data
-        }).catch(error => {
-            console.log(error);
+
+    let activity;
+    let details;
+    let unixTime;
+    let state;
+
+    $: currentTime = new Date().getTime();
+    $: timeSince = currentTime - unixTime;
+    $: time = new Date(timeSince);
+    $: hours = time.getHours();
+    $: minutes = "0" + time.getMinutes();
+    $: seconds = "0" + time.getSeconds();
+    
+
+    onMount(() => {
+		const interval = setInterval(() => {
+			currentTime = new Date().getTime();
+		}, 1000);
     });
 
-    const getLanyardData = async () => {
-        const a = await fetchLanyardData;
-        console.log(a);
-        return a;
-    };
+    onMount(async () => {
+        const response = await fetch("https://api.lanyard.rest/v1/users/420043923822608384")
+        const data = await response.json();
+        console.log(data);
+        activity = data.data.activities[1].name.toUpperCase();
+        details = data.data.activities[1].details;
+        state = data.data.activities[1].state;
+        unixTime = data.data.activities[1].timestamps.start;
+    });
 
-    const data = getLanyardData();
-    
+
+
 </script>
 
 <section>
-    <h4>hi </h4>
-    <h2>Jungle by <br/>
-    CIX HELLO Chapter 3: Hello, Stranger</h2>
-    <br />
-    <h2>1:11 - 3:14 </h2>
+    <h4>
+        {#if activity === undefined}
+            JUST LIVING
+        {:else}
+            {activity}
+        {/if}
+    </h4>
+    <h2>
+        {#if details === undefined}
+            guhhhhh
+            <br />
+        {:else}
+            {details}
+            <br />
+        {/if}
+
+        {#if state === undefined}
+            i just be j
+        {:else}
+            {state}
+            <br />
+        {/if}
+
+        {#if timeSince === undefined}
+            <span></span>
+        {:else}
+            <!-- {currentTime}
+            {unixTime}
+            {hours}:{minutes.substr(-2)}:{seconds.substr(-2)} elapsed -->
+        {/if}
+    </h2>
 </section>
 
 
@@ -31,11 +73,12 @@
     h4 {
         background-color: var(--grey-one);
         border-radius: 100px;
+        font-weight:500;
         padding: 0.5rem 1.5rem;
         width:fit-content;
-        font-size:1.5rem;
-        transform: translateX(-13%);
-        margin-bottom: 0.75rem;
+        font-size:1.4rem;
+        transform: translateX(-5%);
+        margin-bottom: 0.25rem;
     }
 
     h2 {
