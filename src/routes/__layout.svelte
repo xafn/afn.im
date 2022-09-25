69534631fc3a37f89e5442f11d85cd00520aabf9
afnzmn <afnzmn@gmail.com>
2022-09-25 18:32:45 -0400

    guh

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,12 +2,12 @@
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<meta name="og:title" content="afn"/>
+	<meta name="og:title" content="afn.lol"/>
 	<meta content="/default.png" property="og:image"/>
-	<meta property="og:description" content="The only portfolio to exist.">
+	<meta property="og:description" content="the only website ever">
 	<meta name="twitter:image" itemprop="image" content="/default.png">
     <meta name="twitter:card" content="summary">
-    <meta name="theme-color" content="#120e0e">
+    <meta name="theme-color" content="#CCE2F2">
 	<title>afn</title>
 </svelte:head>
 
