c320a9b04144a6fa66088b6ff82069c9c19e11a4
afn <hey@afn.lol>
2022-11-18 23:36:30 -0500

    feat: error page (please work)

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { page } from '$app/stores';
+	import { error } from '@sveltejs/kit';
+
+	$: status = $page.status;
+</script>
+
+<div class="wrapper">
+    <h1>{status}</h1>
+    <h6>{$page.error.message}</h6>
+</div>
+
+
+<style>
+    div {
+        height: calc(100vh - 70px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
