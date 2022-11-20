faea082b5c7a656eba0a5236b67e5986cb65d855
afn <hey@afn.lol>
2022-11-19 22:05:36 -0500

    feat: cooler navbar

--- src/lib/components/molecules/NavHost.svelte
+++ src/lib/components/molecules/NavHost.svelte
@@ -3,18 +3,25 @@
 	let y: number;
 </script>
 
-<nav class:scrolled={y > 20}>
-	<ul>
-		<Nav href="#home" section="home" isSelected={y < 350} />
-		<Nav href="#about" section="about" isSelected={y > 350 && y < 675} />
-		<Nav href="#aw" section="work" isSelected={y > 675} />
-	</ul>
-</nav>
+<span>
+	<nav class:scrolled={y > 20}>
+		<ul>
+			<Nav href="#home" section="home" isSelected={y < 350} />
+			<Nav href="#about" section="about" isSelected={y > 350 && y < 675} />
+			<Nav href="#aw" section="work" isSelected={y > 675} />
+		</ul>
+	</nav>
+</span>
 
 <!-- this is the coolest thing ever wtf?? -->
 <svelte:window bind:scrollY={y} />
 
 <style>
+	span {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 	nav {
 		width: 100%;
 		text-align: center;
@@ -22,12 +29,12 @@
 		position: fixed;
 		top: 0;
 		padding: 1.25rem 0;
-		z-index: 999;
-		background-color: #181414bb;
+		z-index: 99;
+		background-color: var(--bg-color);
 		backdrop-filter: blur(15px);
 		-webkit-backdrop-filter: blur(15px);
 		background-blend-mode: overlay;
-		transition: all 0.2s ease;
+		transition: all 0.5s ease;
 	}
 
 	nav ul {
@@ -37,9 +44,16 @@
 	}
 
 	@media (min-width: 869px) {
+		nav {
+			width: 48rem;
+			border-radius: 12px;
+		}
 		.scrolled {
-			border-bottom: 1.5px solid var(--grey-one);
-			padding: 0.9rem 0;
+			border-bottom: 1.5px solid var(--yellow-opacity);
+			padding: 0.5rem 0;
+			margin-top: 0.5rem;
+			width: 42rem;
+			background-color: #1c1818cf;
 		}
 	}
 
