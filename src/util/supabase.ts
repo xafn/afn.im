256c046d1c966caed4f1c44f88019f5845b156a8
afn <hey@afn.im>
2026-03-23 19:22:52 -0400

    fix: use env variables

--- src/util/supabase.ts
+++ src/util/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 
-const supabaseUrl = PUBLIC_SUPABASE_URL?.trim();
-const supabaseAnonKey = PUBLIC_SUPABASE_ANON_KEY?.trim();
+const supabaseUrl = env.PUBLIC_SUPABASE_URL?.trim();
+const supabaseAnonKey = env.PUBLIC_SUPABASE_ANON_KEY?.trim();
 const hasSupabaseConfig = Boolean(supabaseUrl && supabaseAnonKey);
 
 export const supabase = hasSupabaseConfig
